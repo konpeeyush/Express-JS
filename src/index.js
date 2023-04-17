1a253e8f8cacdfa60ba63425d54870002407d72a
konpeeyush <piyushksingh2589@gmail.com>
2023-04-17 18:41:16 +0530

    Adding changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const public=path.join(__dirname, '../public');
+const public = path.join(__dirname, '../public');
 
-app.use(express.static(public));
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// We have to set the view engine
+app.set("view engine", "hbs");
+app.set('views', path.join(__dirname, '../views')); //!important
+
+// app.use(express.static(public));
+
+// Template engine is used to render dynamic content
+app.get("/", (req, res) => {
+    res.render("index",{
+        title: "Hurray this is rendered using hbs"
+    });
 });
 
 app.listen(8000, () => {
